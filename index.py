e5e8a4878ff592e9aecdd05260ee95ad5e79dfe6
JCDJulian <julian.deocampo@gmail.com>
2017-04-04 22:14:08 -0700

    Render searchOptions page on index, add and initialize MySQL dependency. Download Flask-MySQL here: https://flask-mysql.readthedocs.io/en/latest/

--- index.py
+++ index.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+
 index = Flask(__name__)
+
+# Uncomment and replace w/ remote DB for prod
+# index.config['MYSQL_DATABASE_USER'] = 'root'
+# index.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+# index.config['MYSQL_DATABASE_DB'] = 'dbName'
+# index.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(index)
 
 @index.route('/')
 def home_page():
-   return render_template('home.html')
+   return render_template('searchOptions.html')
 
 if __name__ == '__main__':
    index.run(debug = True, port = 8080)
